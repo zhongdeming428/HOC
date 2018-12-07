2c23e479f53b869a754309b01c82faa01c9f9caa
zhongdeming428 <570160841@qq.com>
2018-12-07 22:49:28 +0800

    update

--- src/decorators/CursorPointer.js
+++ src/decorators/CursorPointer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default Component => class extends React.Component {
   render() {
-    return <div style={{cursor: 'pointer'}}>
+    return <div style={{cursor: 'pointer', display: 'inline-block'}}>
       <Component/>
     </div>
   }
